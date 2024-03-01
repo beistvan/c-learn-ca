f0329298e2754a0267e473121e6b12ae1600e205
Beregszászi István <beistvan@gmail.com>
2024-03-01 20:30:56 +0100

    Comments In C

---
+++ comments-in-c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // Single line comment
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* Multiple line
+   * comment */
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
