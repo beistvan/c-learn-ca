44aaa3dd5e74c3290101b7119515548f5009157e
Beregszászi István <beistvan@gmail.com>
2024-03-01 20:36:31 +0100

    Hello World - complie, C

---
+++ hello_world_complie.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  // output a line
+  printf("Hello World!\n");
+  /* 
+  Compile:
+  gcc hello_world_compile.c -o hello_world_compile
+
+  Run:
+  ./hello_world_compile
+  */ 
+}
