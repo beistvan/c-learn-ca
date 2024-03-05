956793d3c646e43b81645defdf23d309b49e31b3
Beregszászi István <beistvan@gmail.com>
2024-03-05 09:56:09 +0100

    Casting Types In C

---
+++ casting-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  
+  double testScore = 95.7;
+  int displayScore = (int)testScore;
+
+
+  // No need to change below here
+  printf("Great work, you got a %d%% on your test\n", displayScore);
+
+}
+
+// Output:
+// Great work, you got a 95% on your test
