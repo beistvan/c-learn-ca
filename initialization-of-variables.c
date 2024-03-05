60870c5c707a8403426a7d46e5b418e9abb67142
Beregszászi István <beistvan@gmail.com>
2024-03-05 09:49:02 +0100

    Initialization Of Variables

---
+++ initialization-of-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+  int numOfBooks = 1;
+  char favLetter = 'A';
+  char favDigit ='5';
+  double costOfCandyBar = 12.34;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
