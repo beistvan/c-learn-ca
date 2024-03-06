b5777e480950e6c7d8a89cee61861fc0f5a18af7
Beregszászi István <beistvan@gmail.com>
2024-03-06 16:42:35 +0100

    Character Access And Modification

---
+++ character-access-and-modification.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char p[] = "procezzor";
+  // Checkpoint 1 code goes here
+  printf("%c\n", p[2]);
+  // Checkpoint 2 code goes here
+  p[5] = p[6] = 's';
+  printf("%s", p);
+}
