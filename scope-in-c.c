e4881a8e88ca44c156e8de6644312171b9a3ae10
Beregszászi István <beistvan@gmail.com>
2024-03-07 08:17:07 +0100

    Scope In C

---
+++ scope-in-c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char* myMessage = "This is my global message!";
+
+void myFunc() {
+  char* myMessage = "This is my local message!";
+  printf("%s\n", myMessage);
+}
+
+int main() {
+  // You can change `myNumber` to be larger than `50`
+  int myNumber = 39;
+
+  if (myNumber <= 50) {
+    printf("%d\n", myNumber);
+    printf("%s\n", myMessage);
+  } else {    
+    int myNumber = 500;
+    printf("%d\n", myNumber);
+    myFunc();
+  }
+}
