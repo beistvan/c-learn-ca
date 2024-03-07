db342abfa0d69f7dd115a62f7c49f15b1bf88d5d
Beregszászi István <beistvan@gmail.com>
2024-03-07 08:08:35 +0100

    Define Function Prototypes

---
+++ define-function-prototypes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// function prototypes
+int upperFunction(int, int);
+void lowerFunction(char*);
+
+int upperFunction(int number1, int number2){
+  int sum = number1 + number2;
+  lowerFunction("The numbers have been added.");
+  return sum;
+}
+
+void lowerFunction(char* string1) {
+  printf("%s\n", string1);
+} 
+
+int main(){
+  int sum = upperFunction(4, 2);
+}
